97fe9e8783388b105cc514f904741c43b9d1df35
DimitriNakos <dimi1nakos@gmail.com>
2020-12-31 13:21:11 -0500

    Updates

--- src/components/card/card.component.jsx
+++
@@ -1,9 +0,0 @@
-import './card.styles.css'
-
-export const Card = props => (
-    <div className='card-container'>
-        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster"/>
-        <h2> {props.monster.name} </h2>
-        <p> {props.monster.username} : {props.monster.email} </p>
-    </div>
-)
